4196ab10327699f7a4d2617113d4786cfcd60c07
FrancescaLoria <francesca.loria90@hotmail.it>
2023-05-26 17:00:45 +0200

    add Francesca

---
+++ database/seeders/PizzasTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Pizza;
+use Faker\Generator as Faker;
+
+class PizzasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10 ; $i++) { 
+
+            $pizza = new Pizza();
+            $pizza->flavour = $faker->randomElement(['Margherita', 'Capricciosa', 'Diavola', 'Boscaiola', 'prucola', 'Tonno', 'Marinara']);
+            $pizza->ingredients = $faker->randomElement(['Mozzarella', 'Pomodoro', 'Rucola', 'Tonno', 'Wustel', 'funghi', 'Gorgonzola',]);
+            $pizza->price = $faker->randomFloat(2, 5, 15);
+            $pizza->is_available = $faker->randomElement([true, false]);
+            $pizza->is_vegan = $faker->randomElement([true, false]);
+            $pizza->save();
+        }
+    }
+}
