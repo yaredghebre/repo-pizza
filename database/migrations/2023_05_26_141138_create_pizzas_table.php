78aa6c7e3be2c3eb1df58c4920597fb03580688f
Paolacocc <coccettipaola@gmail.com>
2023-05-26 16:22:04 +0200

    Add Paola

---
+++ database/migrations/2023_05_26_141138_create_pizzas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id();
+            $table->string('flavour', 50);
+            $table->string('ingredients', 50);
+            $table->decimal('price', 4, 2);
+            $table->boolean('is_available')->default(true);
+            $table->boolean('is_vegan')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizzas');
+    }
+};
