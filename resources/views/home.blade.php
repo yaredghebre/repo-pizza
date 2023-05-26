98a80a2a1fe58fb18043f44317f56780f2f2e758
silvanoguel <silvanoguel@msn.com>
2023-05-26 17:34:40 +0200

    add:cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>ciao</h1>
+    <div class="container">
+        <h1>Le nostre PIZZE</h1>
+
+        <div class="row row-cols-4">
+            @foreach ($pizzas as $pizza)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $pizza->flavour }}
+                            <h6 class="card-subtitle mb-2 text-body-secondary">${{ $pizza->price }}</h6>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </div>
 @endsection
