4b7f95dcf413edd55c3c3eeff67426f2067c94f5
Massimiliano-Novello <massinov5@gmail.com>
2023-05-31 12:40:14 +0200

    add index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,6 @@
 
 @section('content')
     <div class="container">
-        <h1>Le nostre PIZZE</h1>
-
-        <div class="row row-cols-4">
-            @foreach ($pizzas as $pizza)
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $pizza->flavour }}
-                            <h6 class="card-subtitle mb-2 text-body-secondary">${{ $pizza->price }}</h6>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-
+        <a href="{{route('pizzas.index')}}"><h1>Le nostre pizze</h1></a>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
-use App\Http\Controllers\Guest\HomeController;
+use App\Http\Controllers\Admin\PizzaController;
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']) -> name('home');
+Route::resource('pizzas', PizzaController::class);
